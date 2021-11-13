fc5146836d9147de7e600ec5c86841286df60912
balu srighakolapu <deepaksbss@gmail.com>
2021-11-13 13:46:47 +0530

    internaloperationmcommit2

--- sample1.py
+++ sample1.py
@@ -4,13 +4,4 @@ def decor1(func):
         return x*x
     return inner
 def decor(func):
-    def inner():
-        x=func()
-        return 2*x
-    return inner
-
-@decor1
-@decor
-def num():
-    return 10
-print(num())
+   
