bd15bf2306c003a97ec32116d4297b16651f0d0d
balu srighakolapu <deepaksbss@gmail.com>
2021-11-23 19:35:25 +0530

    New files

--- FilePratice.py
+++ FilePratice.py
@@ -6,7 +6,22 @@ def searchName():
     for line in out:
         if scr == line[1]:
             print(line)
-    
+
+    def searchName():
+        scr = input('enter the name of the employee to retrive the record')
+        f = open('afilecsv.csv', 'r+')
+        out = csv.reader(f)
+        for line in out:
+            if scr == line[1]:
+                print(line)
+
+                def searchName():
+                    scr = input('enter the name of the employee to retrive the record')
+                    f = open('afilecsv.csv', 'r+')
+                    out = csv.reader(f)
+                    for line in out:
+                        if scr == line[1]:
+                            print(line)
 def searchsalary():
     scr= input('enter the employe salary')
     f = open('afilecsv.csv', 'r+')
